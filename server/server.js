26f49a6acd05a1251988db2377b962a49f9646e9
LPT <lptalty@gmail.com>
2021-07-27 22:55:07 -0400

    I believe that webpack is working and styles are working with css

--- server/server.js
+++ server/server.js
@@ -9,16 +9,8 @@ const PORT = 3000;
 
 const app = express();
 
-// const mongoURI = 'mongodb+srv://Liam:personalproject@cluster0.vqipd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-// mongoose.connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => {console.log('MongoDB Connected')})
-// .catch(err => console.log(err))
-
-app.get('/', charityController.getAllUsers, (req, res) => {
+app.get('/data', charityController.getAllUsers, (req, res) => {
 
     /**
     * Since we set `ejs` to be the view engine above, `res.render` will parse the
@@ -26,7 +18,7 @@ app.get('/', charityController.getAllUsers, (req, res) => {
     * a string of proper HTML which will be sent to the client!
     */
     
-    res.status(200).send('You are doing great!')
+    res.status(200).json(res.locals.users)
   
   });
 
